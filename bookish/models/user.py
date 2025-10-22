cbc70882e4f746a0f9e6493bb925d695c7901f79
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-22 15:50:27 +0300

    Add password hashing

--- bookish/models/user.py
+++ bookish/models/user.py
@@ -1,5 +1,6 @@
 from bookish.app import db
 from bookish.models.user_role import Role
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
@@ -17,6 +18,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def serialize(self):
         return {
