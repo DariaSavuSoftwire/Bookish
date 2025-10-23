4aa779241a15b6f8ad396e1ec36fcca3d9989915
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-23 11:37:13 +0300

    Modify add author call

--- bookish/controllers/__init__.py
+++ bookish/controllers/__init__.py
@@ -1,3 +1,4 @@
+from bookish.controllers.author_controller import author_routes
 from bookish.controllers.bookish import bookish_routes
 from bookish.controllers.user_controller import user_routes
 from bookish.controllers.book_controller import book_routes
@@ -6,3 +7,4 @@ from bookish.controllers.book_controller import book_routes
 def register_controllers(app):
     user_routes(app)
     book_routes(app)
+    author_routes(app)
--- bookish/controllers/author_controller.py
+++ bookish/controllers/author_controller.py
@@ -1,5 +1,5 @@
-from flask import request
-from flask_jwt_extended import jwt_required
+from flask import request, jsonify
+from flask_jwt_extended import jwt_required,get_jwt
 from bookish.models import Author
 from bookish.models import db
 
@@ -7,12 +7,17 @@ from bookish.models import db
 def author_routes(app):
     @app.route('/author/get_all', methods=['GET'])
     @jwt_required()
-    def get_all_books():
+    def get_all_authors():
         return {"books": Author.query.all()}
 
     @app.route('/author/add', methods=['POST'])
     @jwt_required()
     def add_author():
+        role=get_jwt().get('role')
+
+        if role!= "ADMIN":
+            return jsonify({"msg": "You are not authorized to perform this action"}), 400
+
         name = request.json.get('name')
 
         if Author.query.filter_by(name=name).first():
