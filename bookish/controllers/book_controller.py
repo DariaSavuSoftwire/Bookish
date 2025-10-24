9c68acdf2bfebf8af6a7311b8e2ac2a03a1f7fbb
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-24 11:17:48 +0300

    Add extra api calls for book update and paged get

--- bookish/controllers/book_controller.py
+++ bookish/controllers/book_controller.py
@@ -10,7 +10,10 @@ def book_routes(app):
     @app.route('/book/get_all', methods=['GET'])
     @jwt_required()
     def get_all_books():
-        sorted_books = Book.query.order_by(Book.title).all()
+        page = request.args.get('page', 1, type=int)
+        elements_per_page = request.args.get('elements_per_page', 3, type=int)
+        offset = (page - 1) * elements_per_page
+        sorted_books = Book.query.order_by(Book.title).limit(elements_per_page).offset(offset).all()
         return jsonify({"books": [book.serialize() for book in sorted_books]}), 200
 
     @app.route('/book/create', methods=['POST'])
@@ -32,19 +35,66 @@ def book_routes(app):
         try:
             new_book = Book(isbn, title, copies_owned)
             db.session.add(new_book)
-            db.session.commit()
+            db.session.flush()
             for author_name in author_names:
                 author = Author.query.filter_by(name=author_name).first()
                 if not author:
                     author = Author(name=author_name)
                     db.session.add(author)
-                    db.session.commit()
+                    db.session.flush()
 
                 book_author = BookAuthors(ISBN=new_book.ISBN, author_id=author.id)
                 db.session.add(book_author)
             db.session.commit()
 
             return jsonify({"message": "Book created successfully"}), 201
+        except Exception as e:
+            return jsonify({"message": str(e)}), 500
+
+    @app.route('/book/update', methods=['PUT'])
+    @jwt_required()
+    def update_book():
+        authenticated_user = get_jwt()
+        user_role = authenticated_user.get('role')
+
+        if user_role != 'ADMIN':
+            return jsonify({"message": "You are not authorized to perform this action"}), 403
+
+        isbn = request.json.get('ISBN')
+        title = request.json.get('title')
+        author_names = request.json.get('authors', [])
+        copies_owned = request.json.get('copies_owned', 0)
+        current_book = Book.query.filter_by(ISBN=isbn).first()
+
+        if not current_book:
+            return jsonify({"message": "The requested book does not exist"}), 404
+
+        try:
+            current_book.title = title
+            current_book.copies_owned = copies_owned
+            new_authors = set(author_names)
+            current_authors = set([author.name for author in current_book.authors])
+
+            authors_to_remove = current_authors - new_authors
+            for author_name in authors_to_remove:
+                author = Author.query.filter_by(name=author_name).first()
+                if author:
+                    book_author = BookAuthors.query.filter_by(ISBN=current_book.ISBN, author_id=author.id).first()
+                    if book_author:
+                        db.session.delete(book_author)
+
+            authors_to_add = new_authors - current_authors
+            for author_name in authors_to_add:
+                author = Author.query.filter_by(name=author_name).first()
+                if not author:
+                    author = Author(name=author_name)
+                    db.session.add(author)
+                    db.session.flush()
+                book_author = BookAuthors(ISBN=current_book.ISBN, author_id=author.id)
+                db.session.add(book_author)
+
+            db.session.commit()
+            return jsonify({"message": "Book updated successfully"}), 200
         except Exception as e:
             return jsonify({"message": str(e)}), 500
 
@@ -64,17 +114,13 @@ def book_routes(app):
         books = query.all()
         return jsonify({"books": [filtered_book.serialize() for filtered_book in books]}), 200
 
-    @app.route('/book/filter_title', methods=['GET'])
-    @jwt_required()
-    def filter_title():
-        title = request.json.get('title')
-        books = Book.query.filter(title in Book.title)
-        return jsonify({"books": books}), 200
-
     @app.route('/book/get_available_books', methods=['GET'])
     @jwt_required()
     def get_available_books():
-        books = Book.query.all()
+        page = request.args.get('page', 1, type=int)
+        elements_per_page = request.args.get('elements_per_page', 10, type=int)
+        offset = (page - 1) * elements_per_page
+        books = Book.order_by(Book.title).limit(elements_per_page).offset(offset).all()
         available_books = []
         for book in books:
             borrowed_copies = BookLoan.query.filter_by(ISBN=book.ISBN).order_by(BookLoan.due_return.asc()).all()
@@ -87,5 +133,4 @@ def book_routes(app):
                 available_book.set_user_to_return(next_return.username)
 
             available_books.append(available_book)
-
         return jsonify({"available_books": [available_book.serialize() for available_book in available_books]}), 200
